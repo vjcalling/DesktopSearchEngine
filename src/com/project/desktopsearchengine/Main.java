f3c4c60ab644a014dfc048dbc41a529a564dcaf8
vjcalling <vibhor.jain@outlook.com>
2017-01-08 22:27:49 +0530

    logging

--- src/com/project/desktopsearchengine/Main.java
+++ src/com/project/desktopsearchengine/Main.java
@@ -2,11 +2,17 @@ package com.project.desktopsearchengine;
 
 import java.util.ArrayList;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 import com.project.desktopsearchengine.exceptions.InvalidPropertyException;
 import com.project.desktopsearchengine.utilities.Utilities;
 
 public class Main {
-
+	
+	//private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+	//public final static Logger logger = Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) {
 
 		try {
