2c11e4ee6b24c47875c0d3220d3200ad3a74fdca
vjcalling <vibhor.jain@outlook.com>
2017-01-13 13:57:02 +0530

    fixing imports

--- src/com/project/desktopsearchengine/MainHandler.java
+++ src/com/project/desktopsearchengine/MainHandler.java
@@ -1,6 +1,5 @@
 package com.project.desktopsearchengine;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,9 +7,7 @@ import java.util.logging.Logger;
 import org.apache.commons.io.FilenameUtils;
 
 import com.project.desktopsearchengine.exceptions.InvalidPropertyException;
-import com.project.desktopsearchengine.files.CSV;
 import com.project.desktopsearchengine.files.FileHandler;
-import com.project.desktopsearchengine.files.PDF;
 import com.project.desktopsearchengine.utilities.Utilities;
 
 public class MainHandler {
