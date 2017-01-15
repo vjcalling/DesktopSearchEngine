a992ae03339b01ab37fa645a7ff2346fb51be96d
vjcalling <vibhor.jain@outlook.com>
2017-01-15 14:50:37 +0530

    adding ranking logic

--- src/com/project/desktopsearchengine/OperationsHandler.java
+++ src/com/project/desktopsearchengine/OperationsHandler.java
@@ -4,10 +4,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
